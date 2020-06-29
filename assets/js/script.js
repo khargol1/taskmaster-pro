f6a53f28fecb431b18ac2b1159c067ab9ef74466
Khargol <josephklein3@gmail.com>
2020-06-28 20:49:29 -0400

    feature/bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,6 +73,13 @@ $(".list-group").on("blur", "textarea", function () {
 
   tasks[status][index].text = text;
   saveTasks();
+  // recreate p element
+var taskP = $("<p>")
+.addClass("m-1")
+.text(text);
+
+// replace textarea with p element
+$(this).replaceWith(taskP);
 });
 
 // due date was clicked
